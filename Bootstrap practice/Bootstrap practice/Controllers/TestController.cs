b0eb4f7cc02093195a9f6ad479aeca887797b1a0
Rayhan <rayhanchowdhury599@gmail.com>
2023-02-24 02:28:03 +0600

    Bootstrap practice

---
+++ Bootstrap practice/Bootstrap practice/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bootstrap_practice.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ABC() 
+        {
+            return View();
+        }
+    }
+}
---
+++ Bootstrap practice/Bootstrap practice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bootstrap_practice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
